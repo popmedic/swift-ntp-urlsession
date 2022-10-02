ee908a5c696917ef543a4a2629ebf8040d10ca0f
Kevin Scardina <kevin.scardina@charter.com>
2022-10-01 18:25:48 -0600

    add difference

--- Sources/swiftntp/main.swift
+++ Sources/swiftntp/main.swift
@@ -15,6 +15,7 @@ let request = URL(string: "ntp://time.apple.com")!
 let group = DispatchGroup()
 // create a task using the NTP URL request
 let task = URLSession.shared.dataTask(with: request) { data, _, error in
+    let localDate = Date()
     // make sure there are no errors and data is not nil
     guard error == nil, let data = data else {
         fatalError("error should be nil not \"\(error!)\" and data should not be nil")
@@ -22,8 +23,9 @@ let task = URLSession.shared.dataTask(with: request) { data, _, error in
     // read the bytes in data into a Date object
     let date = data.withUnsafeBytes { $0.load(as: Date.self) }
     // print the system current date and the date we just got
-    print(tty.wrap("Localhost Date: \(Date().description(with: .current))", inColor: .cyan))
+    print(tty.wrap("Localhost Date: \(localDate.description(with: .current))", inColor: .cyan))
     print(tty.wrap("Apple.com Date: \(date.description(with: .current))", inColor: .green))
+    print(tty.wrap("Difference: \(date.timeIntervalSince1970 - localDate.timeIntervalSince1970)", inColor: .red))
     group.leave()
 }
 // enter the group
